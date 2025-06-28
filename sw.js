b4a85dfc3dea32953fa22c945cf12c7633680bf1
FM <154886003+LFakh@users.noreply.github.com>
2025-06-28 14:42:30 +0100

    Created sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "todo-cache-v1";
+const ASSETS_TO_CACHE = [
+  "./index.html",
+  "./manifest.json",
+  // You might want to include local copies of assets (if available)
+  // Since the CDN assets are already online, caching them is optional.
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
