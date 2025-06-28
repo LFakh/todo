82b7353ebed18a06bac8591e43e90ad82a134b46
FM <154886003+LFakh@users.noreply.github.com>
2025-06-28 15:26:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 const CACHE_NAME = "todo-cache-v1";
 const ASSETS_TO_CACHE = [
-  "./index.html",
-  "./manifest.json",
-  // You might want to include local copies of assets (if available)
-  // Since the CDN assets are already online, caching them is optional.
-];
-
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/favicon.ico'
+  ];
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
